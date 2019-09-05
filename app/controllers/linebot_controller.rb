b77c993e4748332d28de4996bffb578a34ca07e8
azukizawak <omame3388@gmail.com>
2019-09-05 06:57:58 +0000

    15:57

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -11,39 +11,53 @@
        }
      end
      
-     def callback
-       body = request.body.read
+#     def callback
+#      body = request.body.read
+#   
+#       signature = request.env["HTTP_X_LINE_SIGNATURE"]
+#       unless client.validate_signature(body, signature)
+#         error 400 do "Bad Request" end
+#       end
    
-       signature = request.env["HTTP_X_LINE_SIGNATURE"]
-       unless client.validate_signature(body, signature)
-         error 400 do "Bad Request" end
-       end
+#       events = client.parse_events_from(body)
+#   
+#       events.each { |event|
+#         case event
+#         when Line::Bot::Event::Message
+#           case event.type
+#           when Line::Bot::Event::MessageType::Text
+#             message = {
+#               type: "text",
+#               text: event.message["text"] + "!"
+#             }
+#             client.reply_message(event["replyToken"], message)
+#           end
+#         end
+#       }
    
-       events = client.parse_events_from(body)
    
-       events.each { |event|
-         case event
-         when Line::Bot::Event::Message
-           case event.type
-           when Line::Bot::Event::MessageType::Text
-             message = {
-               type: "text",
-               text: event.message["text"] + "!"
-             }
-             client.reply_message(event["replyToken"], message)
-           when Line::Bot::Event::MessageType::Location
-             message = {
-               type: "location",
-               title: "あなたはここにいますか？",
-               address: event.message["address"],
-               latitude: event.message["latitude"],
-               longitude: event.message["longitude"]
-             }
-             client.reply_message(event["replyToken"], message)
-           end
-         end
-       }
-   
+def send(line_ids, message)
+
+   @idol=Idol.offset( rand(Idol.count) ).first
+# Postからランダムで返事を選ぶ。
+
+
+    post('/v1/events', {
+
+            to: line_ids,
+            content: {
+                contentType: ContentType::TEXT,
+                toType: ToType::USER,
+                text: @post.name #返事をPostから取ってくる。
+            },
+
+            toChannel: TO_CHANNEL,
+            eventType: EVENT_TYPE,
+
+
+
+        })
+        
        head :ok
      end
  end
